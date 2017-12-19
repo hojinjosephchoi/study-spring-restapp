f48df2474dd106ab0fec899689f8b7c2b9956f28
hojin.joseph.choi <escape3rd@gmail.com>
2017-12-20 00:07:24 +0900

    JSON Root Element 설정코드 추가

--- src/main/java/devfun/bookstore/rest/config/RestAppConfig.java
+++ src/main/java/devfun/bookstore/rest/config/RestAppConfig.java
@@ -1,11 +1,5 @@
 package devfun.bookstore.rest.config;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.xml.bind.Marshaller;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
@@ -17,86 +11,100 @@ import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.xml.bind.Marshaller;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @EnableWebMvc
 @Configuration
-@ComponentScan(basePackages = { "devfun.bookstore.rest.controller" }, useDefaultFilters = false, includeFilters = { @Filter(Controller.class),  @Filter(ControllerAdvice.class) })
+@ComponentScan(basePackages = {"devfun.bookstore.rest.controller"}, useDefaultFilters = false, includeFilters = {@Filter(Controller.class), @Filter(ControllerAdvice.class)})
 public class RestAppConfig implements WebMvcConfigurer {
 
-	@Override
-	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		configurer
-			.useRegisteredExtensionsOnly(false)
-			.favorPathExtension(true)	//xxx.xml, xxx.json
-			.favorParameter(true)		//xxx?format=xml, xxx?format=json
-			.ignoreAcceptHeader(false)
-			.defaultContentType(MediaType.APPLICATION_JSON)
-			.mediaType("json", MediaType.APPLICATION_JSON)
-			.mediaType("xml", MediaType.APPLICATION_XML);
-	}
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer
+                .useRegisteredExtensionsOnly(false)
+                .favorPathExtension(true)    //xxx.xml, xxx.json
+                .favorParameter(true)        //xxx?format=xml, xxx?format=json
+                .ignoreAcceptHeader(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+    }
 
-	@Override
-	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-		converters.add(mappingJacksonHttpMessageConverter());
-		converters.add(marshallingHttpMessageConverter());
-	}
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(mappingJacksonHttpMessageConverter());
+        converters.add(marshallingHttpMessageConverter());
+    }
 
-	@Bean
-	public MappingJackson2HttpMessageConverter mappingJacksonHttpMessageConverter() {
-		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-		converter.setPrettyPrint(true);
-		return converter;
-	}
-	
-	@Bean 
-	public MarshallingHttpMessageConverter marshallingHttpMessageConverter() {
-		MarshallingHttpMessageConverter converter = new MarshallingHttpMessageConverter();
-		converter.setMarshaller(jaxb2Marshaller());
-		converter.setUnmarshaller(jaxb2Marshaller());
-		return converter;
-	}
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJacksonHttpMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
 
-	@Bean
-	public Jaxb2Marshaller jaxb2Marshaller()  {
-		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-		marshaller.setPackagesToScan(new String[] {
-				"devfun.bookstore.common.domain",
-				"devfun.bookstore.rest.domain",
-				"org.springframework.hateoas"});
-		
-		Map<String, Object> marshallerProperties = new HashMap<String, Object>();
-		marshallerProperties.put(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-		marshaller.setMarshallerProperties(marshallerProperties);
+//        XML과 같이 JSON에서 Root Element 사용을 위한 설정
+//        ObjectMapper objectMapper = converter.getObjectMapper();
+//        objectMapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
+//        objectMapper.configure(SerializationFeature.WRAP_ROOT_VALUE, true);
 
-		return marshaller;
-	}
+//        @JsonRootName 없이 JAXB 어노테이션의 값을 사용하여 JSON 변환에 이용
+//        JaxbAnnotationModule module = new JaxbAnnotationModule();
+//        objectMapper.registerModule(module);
+
+        converter.setPrettyPrint(true);
+        return converter;
+    }
+
+    @Bean
+    public MarshallingHttpMessageConverter marshallingHttpMessageConverter() {
+        MarshallingHttpMessageConverter converter = new MarshallingHttpMessageConverter();
+        converter.setMarshaller(jaxb2Marshaller());
+        converter.setUnmarshaller(jaxb2Marshaller());
+        return converter;
+    }
+
+    @Bean
+    public Jaxb2Marshaller jaxb2Marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setPackagesToScan(new String[]{
+                "devfun.bookstore.common.domain",
+                "devfun.bookstore.rest.domain",
+                "org.springframework.hateoas"});
+
+        Map<String, Object> marshallerProperties = new HashMap<String, Object>();
+        marshallerProperties.put(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        marshaller.setMarshallerProperties(marshallerProperties);
+
+        return marshaller;
+    }
 
 
-//	@Override
-//	public void configureHandlerExceptionResolvers(
-//			List<HandlerExceptionResolver> exceptionResolvers) {
-//		exceptionResolvers.add(exceptionHandlerExceptionResolver());
-//	}
-//	
-//	@Bean
-//	public ExceptionHandlerExceptionResolver exceptionHandlerExceptionResolver() {
-//		ExceptionHandlerExceptionResolver resolver = new ExceptionHandlerExceptionResolver();
-//		List<HttpMessageConverter<?>> converters = resolver.getMessageConverters();
-//		converters.add(mappingJacksonHttpMessageConverter());
-//		converters.add(marshallingHttpMessageConverter());
-//		resolver.setMessageConverters(converters);
+//    @Override
+//    public void configureHandlerExceptionResolvers(
+//            List<HandlerExceptionResolver> exceptionResolvers) {
+//        exceptionResolvers.add(exceptionHandlerExceptionResolver());
+//    }
 //
-//		ContentNegotiationManagerFactoryBean contentNegotiationManager = new ContentNegotiationManagerFactoryBean();
-//		contentNegotiationManager.addMediaType("json", MediaType.APPLICATION_JSON);
-//		contentNegotiationManager.addMediaType("xml", MediaType.APPLICATION_XML);
-//		contentNegotiationManager.afterPropertiesSet();
-//		resolver.setContentNegotiationManager(contentNegotiationManager.getObject());
+//    @Bean
+//    public ExceptionHandlerExceptionResolver exceptionHandlerExceptionResolver() {
+//        ExceptionHandlerExceptionResolver resolver = new ExceptionHandlerExceptionResolver();
+//        List<HttpMessageConverter<?>> converters = resolver.getMessageConverters();
+//        converters.add(mappingJacksonHttpMessageConverter());
+//        converters.add(marshallingHttpMessageConverter());
+//        resolver.setMessageConverters(converters);
 //
-//		return resolver;
-//	}
+//        ContentNegotiationManagerFactoryBean contentNegotiationManager = new ContentNegotiationManagerFactoryBean();
+//        contentNegotiationManager.addMediaType("json", MediaType.APPLICATION_JSON);
+//        contentNegotiationManager.addMediaType("xml", MediaType.APPLICATION_XML);
+//        contentNegotiationManager.afterPropertiesSet();
+//        resolver.setContentNegotiationManager(contentNegotiationManager.getObject());
+//
+//        return resolver;
+//    }
 
 }
